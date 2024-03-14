4360348b855197398a4bda5750cee658b11e6bf2
Geethasri008 <140805464+Geethasri008@users.noreply.github.com>
2024-03-14 12:25:25 +0530

    texttospeech.py

--- texttospeech.py
+++ texttospeech.py
@@ -1,5 +1,7 @@
 import pyttsx3
-text_speech=pyttsx3.init()
-answer=input("What you want to convert to speech: ")
+text_speech = pyttsx3.init()
+rate = text_speech.getProperty('rate')
+text_speech.setProperty('rate', rate - 10)  # Decrease the rate by 10
+answer = input("What do you want to convert to speech: ")
 text_speech.say(answer)
 text_speech.runAndWait()
